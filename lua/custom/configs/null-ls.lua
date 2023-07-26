8fcfd543d5906d4ff502636518804524a8db094f
krusso <kstn.russo@gmail.com>
2023-07-26 14:37:14 +0300

    set up phpcs, phpstan

--- lua/custom/configs/null-ls.lua
+++ lua/custom/configs/null-ls.lua
@@ -1,5 +1,5 @@
 local present, null_ls = pcall(require, "null-ls")
-local h = require("null-ls.helpers")
+local h = require "null-ls.helpers"
 
 if not present then
   return
@@ -20,52 +20,13 @@ local sources = {
   b.formatting.clang_format,
 
   -- php
-  b.formatting.pint,
-  b.diagnostics.phpmd.with {
-    filetypes = { "php" },
-    generator_opts = {
-      command = "phpmd",
-      args = {
-        "--ignore-violations-on-exit",
-        "-", -- process stdin
-        "json",
-        'phpmd.xml',
-      },
-      format = "json_raw",
-      to_stdin = true,
-      from_stderr = false,
-      check_exit_code = function(code)
-        return code <= 1
-      end,
-      on_output = function(params)
-        local parser = h.diagnostics.from_json {
-          attributes = {
-            message = "description",
-            severity = "priority",
-            row = "beginLine",
-            end_row = "endLine",
-            code = "rule",
-          },
-          severities = {
-            h.diagnostics.severities["error"],
-            h.diagnostics.severities["warning"],
-            h.diagnostics.severities["information"],
-            h.diagnostics.severities["hint"],
-          },
-        }
-        params.violations = params.output
-            and params.output.files
-            and params.output.files[1]
-            and params.output.files[1].violations
-          or {}
-
-        return parser { output = params.violations }
-      end,
-    },
+  b.diagnostics.phpstan.with {
+    extra_args = { "--level", "9" },
   },
-  -- b.diagnostics.phpstan,
-  -- php
-  -- b.diagnostics.sonar
+  b.diagnostics.phpcs.with {
+    extra_args = { "--standard", "/home/kr/.config/phpcs/rules.xml" },
+  },
+  b.formatting.pint,
 }
 
 null_ls.setup {
